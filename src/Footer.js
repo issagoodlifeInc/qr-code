6842f20dafbc1190b1ddb22c68d97d8d881a443c
Lesley <leskimuti@gmail.com>
2022-06-28 02:18:42 +0300

    Solved href issue

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,17 @@
-function Footer(){
-    return(
-        <footer className="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Lesley Kimutai</a>.
-        </footer>
-    )
+function Footer() {
+  return (
+    <footer className="attribution">
+      Challenge by{" "}
+      <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a href="https://www.frontendmentor.io/profile/Leskim">Lesley Kimutai</a>.
+    </footer>
+  );
 }
 export default Footer;
